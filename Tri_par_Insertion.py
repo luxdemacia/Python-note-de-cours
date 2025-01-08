23c5d9333a91ffbee9bb8af9fa3a9c0d68f7a1ed
elhadjibtsk@gmail.com <bblova>
2025-01-08 02:43:41 +0100

    Tri par insertion

--- Tri_par_Insertion.py
+++ Tri_par_Insertion.py
@@ -2,7 +2,6 @@ def tri_par_insertion(Tab):
     # On commence du deuxième élément jusqu’au dernier
     for i in range(1, len(Tab)):
         cle = Tab[i]  # L'élément à insérer
-        print("La valeur de i vaut:",i)
         j = i - 1  #   i - 1 correspond à l'index du dernier élément de la partie triée, qui est juste avant l'élément que nous voulons insérer.
 
         # Déplacer les éléments triés qui sont plus grands que la clé
